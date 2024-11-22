92431beab92fbb70e5d82fa9e3cd95e8e913c4af
Eleriin Kurs <eleriin.kurs@voco.ee>
2024-11-22 11:41:43 +0200

    date

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+const ExpenseDate = (props) =>{
+    const month =props.date.toLocaleString('et-EE',{month:'long'})
+    const day =props.date.toLocaleString('et-EE',{day:'2-digit'})
+    const year =props.date.getFullYear()
+
+    return(
+        <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate
