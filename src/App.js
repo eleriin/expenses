f1d799fe4339b2a239a2006f8e79437290c04da9
Eleriin Kurs <eleriin.kurs@voco.ee>
2024-11-22 10:36:06 +0200

    Create and style ExpenseItem components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
 const App = ()=>{
   return (
     <div className="App">
-     
+     <ExpenseItem/>
+     <ExpenseItem/>
     </div>
   );
 }
