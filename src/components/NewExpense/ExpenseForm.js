e0f30e30420c7a06e05f2ce9247ca290e4232265
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-01-09 16:33:24 +0200

    Vormi andmete saatmise t66tlus

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -18,26 +18,42 @@ const ExpenseForm = (props) => {
         setEnteredDate(event.target.value)
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const expenseData ={
+            title: enteredTitle,
+            price: enteredPrice,
+            date: new Date(enteredDate)
+        }
+        setEnteredTitle('')
+        setEnteredPrice('')
+        setEnteredDate('')
+
+    }
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
                     <input 
                         type='text'
                         onChange={titleChangeHandler}
+                        value={enteredTitle}
                         />
                 </div>
                 <div className='new-expense__control'>
                     <label>Price</label>
                     <input type='number' min='0.01' step='0.01'
                     onChange={priceChangeHandler}
+                    value={enteredPrice}
                     />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
                     <input type='date' min='2024-11-12' max='2026-01-31'
                      onChange={dateChangeHandler}
+                     value={enteredDate}
                     />
                 </div>
             </div>
