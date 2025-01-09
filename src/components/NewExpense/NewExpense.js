7e29920511c7854aa20089c1e1a2b8a93196a185
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-01-09 16:40:31 +0200

    Andmete edastamine

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,16 @@ import ExpenseForm from './ExpenseForm'
 
 
 const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id:Math.random().toString()
+        }
+        console.log(expenseData)
+    }
     return(
         <div className='new-expense'>
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
 
     )
