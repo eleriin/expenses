2c970c6b12d8eeefedc1dc1a64490d87d17ae7f0
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-01-15 19:28:14 +0200

    Eraldi komponendi abil ExpensesList

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,14 +13,16 @@ const ExpenseItem = (props) => {
     }
 
     return (
+        <li>
         <Card className='expense-item'>
             <ExpenseDate date={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
-                <div className='expense-item__amount'>{props.data.amount}</div>
+                <div className='expense-item__amount'>{props.data.amount}
+                </div>
             </div>
-            <button onClick={clickHandler}>Click me</button>
         </Card>
+        </li>
     )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,36 +1,27 @@
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
+import { useState } from 'react'
 import './Expenses.css'
+import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
-import { useState }  from 'react'
-
-
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
-    const [filteredYear, setfilteredYear] = useState(new Date().getFullYear().toString())
+    const [filteredYear, setfilteredYear] = useState('2024')
 
-    const filteredChangeHandler = (filteredYear) => {
-        setfilteredYear(filteredYear)
+    const filterChangeHandler = (year) => {
+        console.log('Year data in Expenses.js' + year)
+        setfilteredYear(year)
         }
 
         const filteredExpenses = props.expenses.filter((expense) =>{
-            return expense.date.getFullYear().toString() ===filteredYear
+            return expense.date.getFullYear() == filteredYear
         })
     
-        let expensesContent = <p>No expenses found</p>
-        
-        if(filteredExpenses.length > 0) {
-            expensesContent = filteredExpenses.map((expense)=>{
-                return <ExpenseItem data ={expense} key={expense.id}/>
-            })
-        }
-    
     return (
+        
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter=
-            {filteredChangeHandler} />
-            
-            {expensesContent}
+            {filterChangeHandler} />
+            <ExpensesList expenses={filteredExpenses}/>
             </Card>
     )
 }
